e312e945de95fe294a3d55350586dab2b05c4600
Avin-ash <aadhiavi57@gmail.com>
2024-05-20 16:44:27 +0530

    first commit

---
+++ src/Components/TopButtons.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const TopButtons = ({ setQuery }) => {
+
+    const cities = [
+        {
+            id: 1,
+            name: 'Tokyo',
+        },
+        {
+            id: 2,
+            name: "Delhi",
+        },
+        {
+            id: 3,
+            name: 'Paris',
+        },
+        {
+            id: 4,
+            name: 'London',
+        },
+        {
+            id: 5,
+            name: 'New York',
+        },
+    ];
+
+
+    return (
+        <div className='flex flex-wrap items=center justify-around my-6'>
+            {
+                cities.map((city) => {
+                    return (
+                        <button key={city.id} className='text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transitio ease-in'
+                            onClick={() => setQuery({ q: city.name })}
+                        >{city.name}</button>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default TopButtons;
